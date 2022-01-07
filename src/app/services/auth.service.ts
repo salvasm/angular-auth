bb0f213a72456fbd5ed93ea0a77680c2ef2eca13
Salvador Sánchez Méndez <salvador.sanchez.mendez@gmail.com>
2022-01-07 18:37:00 +0100

    Auth service modified

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 export interface Config {
     apiUrl: string;
@@ -9,15 +10,44 @@ export interface Config {
   providedIn: 'root'
 })
 export class AuthService {
-    apiUrl = 'assets/config.json';
+    private authApiUrl = '/api/auth';
 
     constructor(private http: HttpClient) { }
 
     login(username: string, password: string) {
-        return this.http.post<any>('/api/auth/login', { username, password }).pipe(data => {
-            localStorage.setItem('currentUser', JSON.stringify(data));
-            console.log(data)
-            return data;
-        });
+        return this.http.post<any>(this.authApiUrl + '/login', { username, password }).pipe(
+            data => {
+                localStorage.setItem('token', JSON.stringify(data));
+                return data;
+            });
+    }
+
+    logout() {
+        localStorage.removeItem('token');
+    }
+
+    public get isUserAuthenticated(): boolean {
+        return (localStorage.getItem('token') !== null);
+    }
+
+    private getServerErrorMessage(error: HttpErrorResponse): string {
+        switch (error.status) {
+            case 401: {
+                return `Unauthorized: ${error.message}`;
+            }
+            case 404: {
+                return `Not Found: ${error.message}`;
+            }
+            case 403: {
+                return `Access Denied: ${error.message}`;
+            }
+            case 500: {
+                return `Internal Server Error: ${error.message}`;
+            }
+            default: {
+                return `Unknown Server Error: ${error.message}`;
+            }
+
+        }
     }
 }
